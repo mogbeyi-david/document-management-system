2fbc5f424b0ea6a7e258b2ec7c73a3321aee0bdd
David Mogbeyi <mogbeyidavid@gmail.com>
2019-02-27 13:50:17 +0100

    Add comments to index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+// Pull in dependencies
 import mongoose from 'mongoose';
 import 'dotenv/config';
 import express from 'express';
 
+// Declare environment variables
 const username = process.env.USERNAME;
 const password = process.env.PASSWORD;
 let database = process.env.DATABASE;
@@ -9,9 +11,9 @@ if (process.env.ENVIRONMENT === 'TESTING') database = process.env.TEST_DATABASE;
 const PORT = process.env.PORT || 3000;
 const app = express();
 
+
+//Attempt connection to remote DB
 const connectionString = `mongodb://${username}:${password}@ds255005.mlab.com:55005/${database}`;
-
-
 mongoose.connect(connectionString, {useNewUrlParser: true})
   .then(() => {
     console.log('Connected to remote DB successfully')
@@ -20,9 +22,11 @@ mongoose.connect(connectionString, {useNewUrlParser: true})
     console.log(error)
   });
 
+// Listen on PORT
 app.listen(PORT, () => {
   console.log(`App is running on port: ${PORT}`);
 });
 
+// Export app
 export default app;
 
