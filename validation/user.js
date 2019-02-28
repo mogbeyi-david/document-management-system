ccdd24ba2179275ce85bc8edacb788b5e35d23ad
David Mogbeyi <mogbeyidavid@gmail.com>
2019-02-28 08:01:10 +0100

    Fixed validation errors

--- validation/user.js
+++ validation/user.js
@@ -3,7 +3,7 @@ import Joi from 'joi';
 function validateUser(user) {
   const schema = {
     firstname: Joi.string().max(255).required('Please enter a Firstname'),
-    lastname: Joi.string().maxlength(255).required('Please enter a Lastname'),
+    lastname: Joi.string().max(255).required('Please enter a Lastname'),
     email: Joi.string().max(255).required('Please enter an email address').email(),
     password: Joi.string().min(6).max(255).regex(/^[a-zA-Z0-9]{3,30}$/).required()
   };
