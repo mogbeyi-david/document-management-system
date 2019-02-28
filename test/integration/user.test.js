b3d11e41a517a937a021980ac1d13c1540b2b9e4
David Mogbeyi <mogbeyidavid@gmail.com>
2019-02-28 01:05:40 +0100

    Added tes for creating a new user

--- test/integration/user.test.js
+++ test/integration/user.test.js
@@ -1,15 +1,15 @@
 import 'babel-polyfill';
-import supertest from 'supertest';
+import request from 'supertest';
 import UserModel from '../../models/user';
 import {User as UserController} from '../../api/v1/controllers';
-import server from '../../index'
+import server from '../../index';
 
 let app;
 
 describe('/api/users', () => {
 
   beforeEach(() => {
-    app = server
+    app = server;
   });
 
   afterEach(async (done) => {
@@ -18,9 +18,9 @@ describe('/api/users', () => {
     done();
   });
 
-  describe('Testing the isUserUnique function', () => {
+  describe('/ User helper functions', () => {
     // Add test case for the isUserUnique() function
-    it('isUserUnique() => it should return true if the user is unique', async (done) => {
+    it('should return true if the user is unique', async (done) => {
       // First, we seed the database with two random users
       await UserModel.collection.insertMany([
         {
@@ -51,11 +51,11 @@ describe('/api/users', () => {
       // Make a call to the isUserUnique method on the payload to check if the user is really unique
       const result = await UserController.isUserUnique(payload);
       expect(result).toBeTruthy();
-      done()
+      done();
     }, 30000);
 
     // Add test case for the isUserUnique() function
-    it('isUserUnique() => it should return false if the user is not unique', async (done) => {
+    it('should return false if the user is not unique', async (done) => {
       // First, we seed the database with two random users
       await UserModel.collection.insertMany([
         {
@@ -86,5 +86,24 @@ describe('/api/users', () => {
       expect(result).toBeFalsy();
       done();
     }, 30000);
+  });
+
+  describe('/POST', () => {
+    // Write tests for the POST endpoint to create a new user
+    it('should return a 400 error if user validation fails', async () => {
+      const payload = {
+        lastname: 'test lastname goes here',
+        email: 'testemail@gmail.com',
+        role: 'REGULAR',
+        password: 'testpassword@12345'
+      };
+      const response = await request(server)
+        .post('/api/v1/users/store')
+        .send(payload);
+
+      expect(response).not.toBeNull();
+      expect(response.status).toBe(400);
+      expect(response.body).toHaveProperty('message');
+    });
   });
 });
