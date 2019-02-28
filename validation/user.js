d5d3725f45b3b7632c124ae2cf17bf5d5096820b
David Mogbeyi <mogbeyidavid@gmail.com>
2019-02-28 07:55:54 +0100

    Fixed joi validation errors

--- validation/user.js
+++ validation/user.js
@@ -2,9 +2,9 @@ import Joi from 'joi';
 
 function validateUser(user) {
   const schema = {
-    firstname: Joi.string().maxlength(255).required('Please enter a Firstname'),
+    firstname: Joi.string().max(255).required('Please enter a Firstname'),
     lastname: Joi.string().maxlength(255).required('Please enter a Lastname'),
-    email: Joi.string().maxlength(255).required('Please enter an email address').email(),
+    email: Joi.string().min(6).max(255).required('Please enter an email address').email(),
     password: Joi.string().regex(/^[a-zA-Z0-9]{3,30}$/).required()
   };
 
