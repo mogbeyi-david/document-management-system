30e727c019a7ef3ce238209f3389329306224b0b
David Mogbeyi <mogbeyidavid@gmail.com>
2019-02-28 15:21:08 +0100

    Created test suite for isUserUnique function

--- test/integration/user.test.js
+++ test/integration/user.test.js
@@ -18,167 +18,176 @@ describe('/api/users', () => {
     done();
   });
 
-  describe('/ User helper functions', () => {
-    // Add test case for the isUserUnique() function
-    it('should return true if the user is unique', async (done) => {
-      // First, we seed the database with two random users
-      await UserModel.collection.insertMany([
-        {
-          firstname: 'firstname goes here',
-          lastname: 'lastname goes here',
+  describe('Helper Functions', () => {
+    describe('Helper function to check if a user already exists', () => {
+      // Add test case for the isUserUnique() function
+      it('should return true if the user is unique', async (done) => {
+        // First, we seed the database with two random users
+        await UserModel.collection.insertMany([
+          {
+            firstname: 'firstname goes here',
+            lastname: 'lastname goes here',
+            email: 'email@gmail.com',
+            role: 'REGULAR',
+            password: 'password@12345'
+          },
+          {
+            firstname: 'another firstname goes here',
+            lastname: 'another lastname goes here',
+            email: 'anotheremail@gmail.com',
+            role: 'REGULAR',
+            password: 'anotherpassword@12345'
+          }
+        ]);
+
+        // Then we make a call to the endpoint to create the a new users
+        const payload = {
+          firstname: 'test firstname goes here',
+          lastname: 'test lastname goes here',
+          email: 'testemail@gmail.com',
+          role: 'REGULAR',
+          password: 'testpassword@12345'
+        };
+
+        // Make a call to the isUserUnique method on the payload to check if the user is really unique
+        const result = await isUserUnique(payload);
+        expect(result).toBeTruthy();
+        done();
+      }, 30000);
+
+      // Add test case for the isUserUnique() function
+      it('should return false if the user is not unique', async (done) => {
+        // First, we seed the database with two random users
+        await UserModel.collection.insertMany([
+          {
+            firstname: 'firstname goes here',
+            lastname: 'lastname goes here',
+            email: 'email@gmail.com',
+            role: 'REGULAR',
+            password: 'password@12345'
+          },
+          {
+            firstname: 'another firstname goes here',
+            lastname: 'another lastname goes here',
+            email: 'anotheremail@gmail.com',
+            role: 'REGULAR',
+            password: 'anotherpassword@12345'
+          }
+        ]);
+
+        // Then we make a call to the endpoint to create the a new users
+        const payload = {
+          firstname: 'test firstname goes here',
+          lastname: 'test lastname goes here',
           email: 'email@gmail.com',
           role: 'REGULAR',
-          password: 'password@12345'
-        },
-        {
-          firstname: 'another firstname goes here',
-          lastname: 'another lastname goes here',
-          email: 'anotheremail@gmail.com',
-          role: 'REGULAR',
-          password: 'anotherpassword@12345'
-        }
-      ]);
-
-      // Then we make a call to the endpoint to create the a new users
-      const payload = {
-        firstname: 'test firstname goes here',
-        lastname: 'test lastname goes here',
-        email: 'testemail@gmail.com',
-        role: 'REGULAR',
-        password: 'testpassword@12345'
-      };
-
-      // Make a call to the isUserUnique method on the payload to check if the user is really unique
-      const result = await isUserUnique(payload);
-      expect(result).toBeTruthy();
-      done();
-    }, 30000);
-
-    // Add test case for the isUserUnique() function
-    it('should return false if the user is not unique', async (done) => {
-      // First, we seed the database with two random users
-      await UserModel.collection.insertMany([
-        {
-          firstname: 'firstname goes here',
-          lastname: 'lastname goes here',
-          email: 'email@gmail.com',
-          role: 'REGULAR',
-          password: 'password@12345'
-        },
-        {
-          firstname: 'another firstname goes here',
-          lastname: 'another lastname goes here',
-          email: 'anotheremail@gmail.com',
-          role: 'REGULAR',
-          password: 'anotherpassword@12345'
-        }
-      ]);
-
-      // Then we make a call to the endpoint to create the a new users
-      const payload = {
-        firstname: 'test firstname goes here',
-        lastname: 'test lastname goes here',
-        email: 'email@gmail.com',
-        role: 'REGULAR',
-        password: 'testpassword@12345'
-      };
-      const result = await isUserUnique(payload);
-      expect(result).toBeFalsy();
-      done();
-    }, 30000);
+          password: 'testpassword@12345'
+        };
+        const result = await isUserUnique(payload);
+        expect(result).toBeFalsy();
+        done();
+      }, 30000);
+    });
   });
 
   describe('/POST', () => {
-    // Write tests for the POST endpoint to create a new user without a firstname
-    it('should return a 400 error if user validation fails', async () => {
-      const payload = {
-        lastname: 'test lastname goes here',
-        email: 'testemail@gmail.com',
-        role: 'REGULAR',
-        password: 'testpassword@12345'
-      };
-      const response = await request(server)
-        .post('/api/v1/user/store')
-        .send(payload);
 
-      expect(response).not.toBeNull();
-      expect(response.status).toBe(400);
-      expect(response.body).toHaveProperty('message');
-      expect(response.body).toHaveProperty('data');
-      expect(response.body.data).toMatchObject(payload);
-    }, 30000);
+    describe('Creating a new user', () => {
+      // Write tests for the POST endpoint to create a new user without a firstname
+      it('should return a 400 error if firstname, lastname, email or password is not provided', async () => {
+        const payload = {
+          lastname: 'test lastname goes here',
+          email: 'testemail@gmail.com',
+          role: 'REGULAR',
+          password: 'testpassword@12345'
+        };
+        const response = await request(server)
+          .post('/api/v1/user/store')
+          .send(payload);
 
-    // Write tests for the POST endpoint to create a new user with a password of less than 6 characters
-    it('should return a 400 error if user validation fails', async () => {
-      const payload = {
-        firstname: 'test firstname goes here',
-        lastname: 'test lastname goes here',
-        email: 'testemail@gmail.com',
-        role: 'REGULAR',
-        password: 'test'
-      };
-      const response = await request(server)
-        .post('/api/v1/user/store')
-        .send(payload);
+        expect(response).not.toBeNull();
+        expect(response.status).toBe(400);
+        expect(response.body).toHaveProperty('message');
+        expect(response.body).toHaveProperty('data');
+        expect(response.body.data).toMatchObject(payload);
+      }, 30000);
 
-      expect(response).not.toBeNull();
-      expect(response.status).toBe(400);
-      expect(response.body).toHaveProperty('message');
-      expect(response.body).toHaveProperty('data');
-      expect(response.body.data).toMatchObject(payload);
-    }, 30000);
-    it('should return a 400 error if user already exists', async () => {
+      // Write tests for the POST endpoint to create a new user with a password of less than 6 characters
+      it('should return a 400 error if password is less than six (6) characters', async () => {
+        const payload = {
+          firstname: 'test firstname goes here',
+          lastname: 'test lastname goes here',
+          email: 'testemail@gmail.com',
+          role: 'REGULAR',
+          password: 'test'
+        };
+        const response = await request(server)
+          .post('/api/v1/user/store')
+          .send(payload);
 
-      await UserModel.collection.insertMany([
-        {
-          firstname: 'firstname goes here',
-          lastname: 'lastname goes here',
+        expect(response).not.toBeNull();
+        expect(response.status).toBe(400);
+        expect(response.body).toHaveProperty('message');
+        expect(response.body).toHaveProperty('data');
+        expect(response.body.data).toMatchObject(payload);
+      }, 30000);
+
+      // Testcase for when a user already exists
+      it('should return a 400 error if user already exists', async () => {
+
+        await UserModel.collection.insertMany([
+          {
+            firstname: 'firstname goes here',
+            lastname: 'lastname goes here',
+            email: 'email@gmail.com',
+            password: 'password@12345'
+          },
+          {
+            firstname: 'another firstname goes here',
+            lastname: 'another lastname goes here',
+            email: 'anotheremail@gmail.com',
+            password: 'anotherpassword@12345'
+          }
+        ]);
+
+        const payload = {
+          firstname: 'test firstname goes here',
+          lastname: 'test lastname goes here',
           email: 'email@gmail.com',
-          password: 'password@12345'
-        },
-        {
-          firstname: 'another firstname goes here',
-          lastname: 'another lastname goes here',
-          email: 'anotheremail@gmail.com',
-          password: 'anotherpassword@12345'
-        }
-      ]);
+          password: 'test@password'
+        };
+        const response = await request(server)
+          .post('/api/v1/user/store')
+          .send(payload);
 
-      const payload = {
-        firstname: 'test firstname goes here',
-        lastname: 'test lastname goes here',
-        email: 'email@gmail.com',
-        password: 'test@password'
-      };
-      const response = await request(server)
-        .post('/api/v1/user/store')
-        .send(payload);
+        expect(response).not.toBeNull();
+        expect(response.status).toBe(400);
+        expect(response.body).toHaveProperty('message', 'User already exists');
+        expect(response.body).toHaveProperty('data');
+        expect(response.body.data).toMatchObject(payload);
+      }, 30000);
 
-      expect(response).not.toBeNull();
-      expect(response.status).toBe(400);
-      expect(response.body).toHaveProperty('message', 'User already exists');
-      expect(response.body).toHaveProperty('data');
-      expect(response.body.data).toMatchObject(payload);
-    }, 30000);
+      // Testcase for successful creation of a user
+      it('should return a 201 if the user details are of the correct format', async () => {
+        const payload = {
+          firstname: 'test firstname goes here',
+          lastname: 'test lastname goes here',
+          email: 'testemail@gmail.com',
+          password: 'test@password'
+        };
+        const response = await request(server)
+          .post('/api/v1/user/store')
+          .send(payload);
 
-    it('should return a 201 if the user details are of the correct format', async () => {
-      const payload = {
-        firstname: 'test firstname goes here',
-        lastname: 'test lastname goes here',
-        email: 'testemail@gmail.com',
-        password: 'test@password'
-      };
-      const response = await request(server)
-        .post('/api/v1/user/store')
-        .send(payload);
+        console.log('RESPONSE: ', response.text);
 
-      console.log('RESPONSE: ', response.text);
+        expect(response).not.toBeNull();
+        expect(response.status).toBe(201);
+        expect(response.body).toHaveProperty('message');
+        expect(response.body).toHaveProperty('data');
+        expect(response.body.data).toHaveProperty('_id');
+      }, 30000);
+    });
 
-      expect(response).not.toBeNull();
-      expect(response.status).toBe(201);
-      expect(response.body).toHaveProperty('message');
-      expect(response.body).toHaveProperty('data');
-      expect(response.body.data).toHaveProperty('_id');
-    }, 30000);
   });
 });
