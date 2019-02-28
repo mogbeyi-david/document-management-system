7e51f73c2bf403f4b18db8a6190083a53e02dfab
David Mogbeyi <mogbeyidavid@gmail.com>
2019-02-28 07:44:19 +0100

    Rearranged route-controller folder structure

--- api/v1/controllers/UserController.js
+++ api/v1/controllers/UserController.js
@@ -1,25 +1,23 @@
 import * as HttpStatus from 'http-status-codes';
-
 import {User as UserModel} from '../../../models';
 import {validateUser} from '../../../validation';
 
-class UserController {
-  async isUserUnique(user) {
-    const email = user.email;
-    const existingUser = await UserModel.find({email: email});
-    return existingUser.length <= 0;
-  }
 
-  store(req, res) {
-    const {error, value} = validateUser(req.body);
-    if (error) {
-      return res.status(HttpStatus.BAD_REQUEST)
-        .send({message: error.details[0].message, data: value});
-    }
-  }
-}
-
-const User = new UserController();
-export {
-  User
+exports.isUserUnique = async function (user) {
+  const email = user.email;
+  const existingUser = await UserModel.find({email: email});
+  return existingUser.length <= 0;
 };
+
+exports.store = function (req, res) {
+  const {error, value} = validateUser(req.body);
+  if (error) {
+    return res.status(HttpStatus.BAD_REQUEST)
+      .send({message: error.details[0].message, data: value});
+  }
+};
+
+// const User = new UserController();
+// export {
+//   User
+// };
--- api/v1/routes/user.js
+++ api/v1/routes/user.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import {UserController} from '../controllers'
 
+
+
 const router = express.Router();
 
 router.post('/store', UserController.store());
