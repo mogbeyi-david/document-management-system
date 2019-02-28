f3931e09d218c1ad3339e0072b5a3c34994358d7
David Mogbeyi <mogbeyidavid@gmail.com>
2019-02-28 07:59:12 +0100

    Added min and max length for password

--- validation/user.js
+++ validation/user.js
@@ -4,8 +4,8 @@ function validateUser(user) {
   const schema = {
     firstname: Joi.string().max(255).required('Please enter a Firstname'),
     lastname: Joi.string().maxlength(255).required('Please enter a Lastname'),
-    email: Joi.string().min(6).max(255).required('Please enter an email address').email(),
-    password: Joi.string().regex(/^[a-zA-Z0-9]{3,30}$/).required()
+    email: Joi.string().max(255).required('Please enter an email address').email(),
+    password: Joi.string().min(6).max(255).regex(/^[a-zA-Z0-9]{3,30}$/).required()
   };
 
   return Joi.validate(user, schema);
