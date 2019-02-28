76330fbfe0b816f72bcb3fa2d5b855c7836cd5a7
David Mogbeyi <mogbeyidavid@gmail.com>
2019-02-28 10:07:54 +0100

    implemented the functionality to successfully create a new user

--- api/v1/controllers/UserController.js
+++ api/v1/controllers/UserController.js
@@ -1,6 +1,8 @@
+import _ from 'lodash';
 import * as HttpStatus from 'http-status-codes';
 import {User as UserModel} from '../../../models';
 import {validateUser} from '../../../validation';
+import hasher from '../../../utils/hash';
 
 
 //Method to determine if an about-to-be-created user is unique, using the email in the payload
@@ -11,10 +13,28 @@ exports.isUserUnique = async function (user) {
 };
 
 //Method to store the new user in the database
-exports.store = function (req, res) {
+exports.store = async function (req, res) {
   const {error, value} = validateUser(req.body);
   if (error) {
     return res.status(HttpStatus.BAD_REQUEST)
       .send({message: error.details[0].message, data: value});
+  }
+
+  try {
+    const newUser = new UserModel({
+      firstname: req.body.firstname,
+      lastname: req.body.lastname,
+      email: req.body.email,
+      role: 'REGULAR',
+      password: await hasher.hashPassword(req.body.password)
+    });
+    const result = await newUser.save();
+    const response = _.pick(result, ['_id', 'firstname', 'lastname', 'email', 'role']);
+    res.status(HttpStatus.CREATED).send({
+      message: 'User created successfully',
+      data: response
+    });
+  } catch (exception) {
+    res.status(HttpStatus.INTERNAL_SERVER_ERROR).send(exception.message);
   }
 };
