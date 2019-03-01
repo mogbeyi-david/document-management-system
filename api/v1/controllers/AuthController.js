202c68692404f7873a3cef6a48a5b84c0bfd9c63
David Mogbeyi <mogbeyidavid@gmail.com>
2019-03-01 02:03:50 +0100

    Added logic to check if the user already exists

--- api/v1/controllers/AuthController.js
+++ api/v1/controllers/AuthController.js
@@ -2,12 +2,21 @@ import _ from 'lodash';
 import * as HttpStatus from 'http-status-codes';
 import {User as UserModel} from '../../../models';
 import {validateLoginPayload} from '../../../validation';
-import hasher from '../../../utils/hash';
+import bcrypt from 'bcrypt';
 
 exports.login = async function (req, res) {
+
+  // Validate the payload using Joi
   const {error, value} = validateLoginPayload(req.body);
   if (error) {
     return res.status(HttpStatus.BAD_REQUEST)
       .send({message: error.details[0].message, data: value});
+  }
+
+  //Check if the user already exists
+  const user = await UserModel.findOne({email: req.body.email});
+  if (!user) {
+    res.status(HttpStatus.BAD_REQUEST)
+      .send({message: 'Invalid Email or Password', data: req.body});
   }
 };
