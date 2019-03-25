8580ed3733009294b670be2fe7ff18c56e1e39a1
David Mogbeyi <mogbeyidavid@gmail.com>
2019-03-25 12:06:02 +0100

    Updated the response format of the auth middleware

--- api/v1/routes/user.js
+++ api/v1/routes/user.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import {UserController} from '../controllers';
 import {AuthController} from '../controllers';
+import {auth} from '../../../middlewares/auth';
 
 const router = express.Router();
 
-router.post('/store', UserController.store);
-router.post('/auth', AuthController.login);
+router.post('/store', UserController.store); // Route to store a new
+
+router.post('/auth', AuthController.login); // Route to login a user
+
+router.get('/', auth, UserController.get); // Route to get all users
 
 export {
   router
